da4e9f4d39d0c8fd69e28143bbe2ef3770d57e79
gatocor <g.torregrosa@outlook.com>
2021-08-04 23:26:21 +0200

    Finished with the interactive plot

--- home/cytoscape/js/index.js
+++ home/cytoscape/js/index.js
@@ -23,28 +23,43 @@
     }
     let applyDatasetFromSelect = () => Promise.resolve( $dataset.value ).then( getDataset ).then( applyDataset );
 
-    let $stylesheet = $('#color');
+    let $color = $('#color');
     let getStylesheet = name => {
       let convert = res => toJson(res);
 
-      return fetch(`stylesheets/stage.json`).then( convert );
+      return fetch(`stylesheets/base.json`).then( convert );
     };
     let applyStylesheet = stylesheet => {
-      if( typeof stylesheet === typeof '' ){
-        cy.style().fromString( stylesheet ).update();
-      } else {
-        cy.style().fromJson( stylesheet ).update();
-      }
-      if ($stylesheet.value == "stage"){
-        var y = "mapData("+$stylesheet.value+",6.5,8.5,blue,red)"
+
+      cy.style().fromJson( stylesheet ).update();
+
+      if ($color.value == "stage"){
+        var y = "mapData("+$color.value+",6.5,8.5,blue,red)"
       }else{
-        var y = "mapData("+$stylesheet.value+",0,1,blue,red)"
+        var y = "mapData("+$color.value+",0,1,blue,red)"
       }
       cy.style().selector("node").style({'background-color':y}).update()
     };
-    let applyStylesheetFromSelect = () => Promise.resolve( $stylesheet.value ).then( getStylesheet ).then( applyStylesheet );
+    let applyStylesheetFromSelect = () => Promise.resolve( $color.value ).then( getStylesheet ).then( applyStylesheet );
 
-    let $layout = $('#layout');
+    let $weights = $('#weights');
+    let getWeights = name => {
+      let convert = res => toJson(res);
+
+      return fetch(`stylesheets/base.json`).then( convert );
+    };
+    let applyWeights = weight => {
+
+      cy.style().fromJson( weight ).update();
+
+      switch ($weights.value){
+        case "weight": var y = "mapData(weight,0,1,blue,red)"; break;
+        case "weight2": var y = 3; break;
+      }
+      cy.style().selector("edge").style({'width':y}).update()
+    };
+    let applyWeightsFromSelect = () => Promise.resolve( $weights.value ).then( getWeights ).then( applyWeights );
+
     let layouts = {
       hierarchical: { // replace with your own layout parameters
         name: 'breadthfirst',
@@ -63,87 +78,79 @@
 
       return l.run().promiseOn('layoutstop');
     }
-    let applyLayoutFromSelect = () => Promise.resolve( $layout.value ).then( getLayout ).then( applyLayout );
-
-    let $algorithm = $('#algorithm');
-    let getAlgorithm = (name) => {
-      switch (name) {
-        case 'bfs': return Promise.resolve(cy.elements().bfs.bind(cy.elements()));
-        case 'dfs': return Promise.resolve(cy.elements().dfs.bind(cy.elements()));
-        case 'astar': return Promise.resolve(cy.elements().aStar.bind(cy.elements()));
-        case 'none': return Promise.resolve(undefined);
-        case 'custom': return Promise.resolve(undefined); // replace with algorithm of choice
-        default: return Promise.resolve(undefined);
-      }
-    };
-    let runAlgorithm = (algorithm) => {
-      if (algorithm === undefined) {
-        return Promise.resolve(undefined);
-      } else {
-        let options = {
-          root: '#' + cy.nodes()[0].id(),
-          // astar requires target; goal property is ignored for other algorithms
-          goal: '#' + cy.nodes()[Math.round(Math.random() * (cy.nodes().size() - 1))].id()
-        };
-        return Promise.resolve(algorithm(options));
-      }
-    }
-    let currentAlgorithm;
-    let animateAlgorithm = (algResults) => {
-      // clear old algorithm results
-      cy.$().removeClass('highlighted start end');
-      currentAlgorithm = algResults;
-      if (algResults === undefined || algResults.path === undefined) {
-        return Promise.resolve();
-      }
-      else {
-        let i = 0;
-        // for astar, highlight first and final before showing path
-        if (algResults.distance) {
-          // Among DFS, BFS, A*, only A* will have the distance property defined
-          algResults.path.first().addClass('highlighted start');
-          algResults.path.last().addClass('highlighted end');
-          // i is not advanced to 1, so start node is effectively highlighted twice.
-          // this is intentional; creates a short pause between highlighting ends and highlighting the path
-        }
-        return new Promise(resolve => {
-          let highlightNext = () => {
-            if (currentAlgorithm === algResults && i < algResults.path.length) {
-              algResults.path[i].addClass('highlighted');
-              i++;
-              setTimeout(highlightNext, 500);
-            } else {
-              // resolve when finished or when a new algorithm has started visualization
-              resolve();
-            }
-          }
-          highlightNext();
-        });
-      }
-    };
-    let applyAlgorithmFromSelect = () => Promise.resolve( $algorithm.value ).then( getAlgorithm ).then( runAlgorithm ).then( animateAlgorithm );
+    let applyLayoutFromSelect = () => Promise.resolve( "hierarchical" ).then( getLayout ).then( applyLayout );
 
     cy = window.cy = cytoscape({
-      container: $('#cy')
+      container: $('#cy'),
+
+      layout: { // replace with your own layout parameters
+          name: 'breadthfirst',
+          directed: true,
+          padding: 30
+      }
     });
 
-    tryPromise( applyDatasetFromSelect ).then( applyStylesheetFromSelect ).then( applyLayoutFromSelect );
+    tryPromise( applyDatasetFromSelect ).then( applyStylesheetFromSelect ).then( applyWeightsFromSelect ).then( applyLayoutFromSelect );
 
     $dataset.addEventListener('change', function(){
-      tryPromise( applyDatasetFromSelect ).then( applyLayoutFromSelect ).then ( applyAlgorithmFromSelect );
+      tryPromise( applyDatasetFromSelect ).then( applyLayoutFromSelect );
     });
 
-    $stylesheet.addEventListener('change', applyStylesheetFromSelect);
+    $color.addEventListener('change', applyStylesheetFromSelect);
 
-    $layout.addEventListener('change', applyLayoutFromSelect);
+    $weights.addEventListener('change', applyWeightsFromSelect);
 
-    $('#redo-layout').addEventListener('click', applyLayoutFromSelect);
+    // $('#redo-layout').addEventListener('click', applyLayoutFromSelect);
 
-    $algorithm.addEventListener('change', applyAlgorithmFromSelect);
+    let $a = $('#hola');
+    cy.bind('click', 'node', function(node) {
+      Promise.resolve($a)
+      console.log($a.value)
+      console.log(node.cyTarget.predecessors().edges());
+      switch($a.value){
+        case "false":
 
-    $('#redo-algorithm').addEventListener('click', applyAlgorithmFromSelect);
+          break;
+        case "backward":
+          node.cyTarget.incomers().edges().animate({
+            style: {
+              lineColor: "red"
+            }
+          });
+          break;
+        case "forward":
+          node.cyTarget.outgoers().edges().animate({
+            style: {
+              lineColor: "red"
+            }
+          });
+          break;
+      }
+    });
+    cy.bind('cxttap', 'node', function(node) {
+      console.log(node.cyTarget.predecessors().edges());
+      node.cyTarget.incomers().edges().animate({
+        style: {
+          lineColor: "lightgrey"
+        }
+      });
+      console.log(node.cyTarget.predecessors().edges());
+      node.cyTarget.outgoers().edges().animate({
+        style: {
+          lineColor: "lightgrey"
+        }
+      });
+    });
+
+    // cy.on("click", "node", (evt) => {evt.cyTarget.ancestors().animate({
+    //   style: {lineColor: "red"} }) })
+    // cy.off("tap", "node", (evt) => {evt.cyTarget.successors().animate({
+    //     style: {lineColor: "grey"} }) })
+  
   });
+  
 })();
+
 
 // tooltips with jQuery
 $(document).ready(() => $('.tooltip').tooltipster());
